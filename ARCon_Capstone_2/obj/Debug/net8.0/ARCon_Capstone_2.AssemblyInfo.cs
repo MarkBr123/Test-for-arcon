b85afdec808e838baab3e65ad6e1a163dc76f2cf
BrizuelaMark <mark.ryanbrizuela27@gmail.com>
2026-02-01 11:57:23 +0800

    sd
    sds

--- ARCon_Capstone_2/obj/Debug/net8.0/ARCon_Capstone_2.AssemblyInfo.cs
+++ ARCon_Capstone_2/obj/Debug/net8.0/ARCon_Capstone_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eb41612fabcc78fa24c4e6a534f640b2aabd726")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da9d3d92b3a4ca829723cc1f3726a99dc6d4f983")]
 [assembly: System.Reflection.AssemblyProductAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
