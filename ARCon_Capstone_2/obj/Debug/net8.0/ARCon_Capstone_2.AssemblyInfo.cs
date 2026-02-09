64d8a6074a7523545b864a096d737d3397a75593
vin-vin-7 <nivramcayobit@zohomail.com>
2026-02-10 01:23:43 +0800

    added the latest DB
    added the latest DB

--- ARCon_Capstone_2/obj/Debug/net8.0/ARCon_Capstone_2.AssemblyInfo.cs
+++ ARCon_Capstone_2/obj/Debug/net8.0/ARCon_Capstone_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd7b11dae76cb87390c512fa785402e6554f98c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b86fdbcbca5636869782dca335de18953f79e9a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
