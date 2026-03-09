977dce2356ba6402eb8be42883e97773a95ae842
vin-vin-7 <nivramcayobit@zohomail.com>
2026-03-10 01:41:27 +0800

    Started Developing (Services)
    Started Developing (Services)

--- ARCon_Capstone_2/obj/Debug/net8.0/ARCon_Capstone_2.AssemblyInfo.cs
+++ ARCon_Capstone_2/obj/Debug/net8.0/ARCon_Capstone_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ae124433f3f0d0b974d049822db756c3816b898")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b6e4c2b499cd930efe6316b46d51d8a2210c934")]
 [assembly: System.Reflection.AssemblyProductAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
