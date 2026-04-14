63458c5bcd710e9f414c4a7d82787265e58f5b4b
SusanoMarcelino <137910837+Susano12345@users.noreply.github.com>
2026-04-14 15:16:01 +0800

    view

--- ARCon_Capstone_2/obj/Debug/net8.0/ARCon_Capstone_2.AssemblyInfo.cs
+++ ARCon_Capstone_2/obj/Debug/net8.0/ARCon_Capstone_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be0a6d4b484228fa6f0a13c09d4f6492efb6471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4770bac767647e368dcf68966934f14e64ace9d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ARCon_Capstone_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
